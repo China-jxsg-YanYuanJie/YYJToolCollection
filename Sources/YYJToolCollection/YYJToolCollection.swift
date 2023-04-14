2ab792e5390f147c8b9a36567740e092b8fbefca
ajay.yan <ajay.yan@anker.io>
2023-04-14 11:12:26 +0800

    [需求] 添加SVG 图片功能.以及log初步打印版本

--- Sources/YYJToolCollection/YYJToolCollection.swift
+++ Sources/YYJToolCollection/YYJToolCollection.swift
@@ -1,6 +1,15 @@
+import SwiftUI
+import UIKit
 public struct YYJToolCollection {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+public struct YYJLog{
+    public static func info(_ message: Any, file: String = #file, method: String = #function, line: Int = #line) {
+      #if DEBUG
+        print("🍺🍺🍺\n\((file as NSString).lastPathComponent)[\(line)], \(method): \n\(message)\n🍺🍺🍺\n")
+      #endif
     }
 }
--- Tests/YYJToolCollectionTests/YYJToolCollectionTests.swift
+++ Tests/YYJToolCollectionTests/YYJToolCollectionTests.swift
@@ -7,5 +7,28 @@ final class YYJToolCollectionTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(YYJToolCollection().text, "Hello, World!")
+        YYJLog.info("xxxx")
+    }
+}
+enum testImage: String, AppImageAssetProtocol {
+    case ic_com_tabbar_home
+    case ic_com_tabbar_product
+    case ic_com_flamingo
+    case ic_com_add
+    func relative() -> String {
+        switch self {
+        case .ic_com_flamingo, .ic_com_add:
+            return "Common/"
+        default:
+            return "TabBar/"
+        }
+    }
+
+    func fullName() -> String {
+        self.rawValue
+    }
+
+    func bundlePath() -> String? {
+        Bundle.main.path(forResource: "ImageResource", ofType: "bundle")
     }
 }
